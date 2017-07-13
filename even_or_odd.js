04ab30380e896386f92a820a0eaf4d5e23bc015a
IamZombiez <dereksiu216@gmail.com>
2017-07-13 16:26:19 -0700

    cleaned up code

--- even_or_odd.js
+++ even_or_odd.js
@@ -10,17 +10,18 @@ function evenOdd(num){
 
 evenOdd(-6)
 
-function oddEven(num){
-   var i = Math.abs(num)
+function oddEven(num) {
+  var i = Math.abs(num)
 
-    while( i >= 0){
-    if(i === 0){
+  while (i >= 0) {
+    if (i === 0) {
       console.log("Even")
-      } if (i === 1) {
-      console.log("Odd")
-      }
-    i -= 2
     }
+    if (i === 1) {
+      console.log("Odd")
+    }
+    i -= 2
+  }
 };
 
 oddEven(7)
