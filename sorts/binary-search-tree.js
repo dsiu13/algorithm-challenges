a0ee61883744acbbf2f5f64e418d65a05055849c
IamZombiez <dereksiu216@gmail.com>
2017-07-19 16:29:00 -0700

    bst update

--- sorts/binary-search-tree.js
+++ sorts/binary-search-tree.js
@@ -1,53 +1,114 @@
-//Tree Node Example
-function Node(val){
-  this.value = val;
-  this.left = null;
-  this.right = null;
+class TreeNode{
+  constructor(data){
+    this.data = data,
+    this.prev = null,
+    this.left = null,
+    this.right = null
+  }
 }
 
-//Create a Constructor
-function BinarySearchTree(){
-  this.root = null;
-}
-
-// Smaller Values are placed Left, Larger Values go Right.
-BinarySearchTree.prototype.push = function(val){
-   var root = this.root;
-
-   if(!root){
-      this.root = new Node(val);
-      return;
-   }
-
-   var currentNode = root;
-   var nextNode = new Node(val);
-
-   while(currentNode){
-      if(val < currentNode.value){
-          if(!currentNode.left){
-             currentNode.left = nextNode;
-             break;
-          }
-          else{
-             currentNode = currentNode.left;
-        }
-     }
-     else{
-         if(!currentNode.right){
-            currentNode.right = nextNode;
-            break;
-         }
-         else{
-            currentNode = currentNode.right;
-         }
-     }
+export default class BinaryTree{
+  constructor(){
+    this.numOfTreeNodes = 1,
+    this.firstCenterNode = new TreeNode(1),
+    this.tree = this.firstCenterNode
   }
 
-};
+  //recursive function to locate appropriate spot for new node.
+  newNodePlacement(data, node, newNode){
+    if(data > node.data || node.data === null){
+      if(node.right === null){
+        newNode.prev = node
+        return node.right = newNode
+      }else{
+        node = node.right
+        return this.newNodePlacement(data, node, newNode)
+      }
+    }else{
+      if(node.left === null){
+        newNode.prev = node
+        return node.left = newNode
+      }else{
+        node = node.left
+        return this.newNodePlacement(data, node, newNode)
+      }
+    }
+  }
+  //recursion function to find a specific node
+  findNode(data, node){
+    if(data === node.data){
+      return node
+    }else if(data > node.data){
+      if(node.right === null){
+        return null
+      }else{
+        node = node.right
+        return this.findNode(data, node)
+      }
+    }else if(data < node.data){
+      if(node.left === null){
+        return null
+      }else{
+        node = node.left
+        return this.findNode(data, node)
+      }
+    }
+  }
+  //walks the tree and finds where its place is amongst the other tree nodes and is inserted accordingly.
+  insert(data){
+    let currentNode = this.firstCenterNode
+    let newNode = new TreeNode(data)
+    this.newNodePlacement(data, currentNode, newNode)
+    this.numOfTreeNodes++
+  }
+  //traverses the tree locating a specific TreeNode or returns null.
+  search(nodeData){
+    let currentNode = this.firstCenterNode
+    return this.findNode(nodeData, currentNode)
+  }
+  //removes a values node from the tree.
+  remove(data){
+    let currentNode = this.firstCenterNode
+    let node = this.findNode(data, currentNode)
+    if(node.right){
+      node.prev.right = node.right
 
-var bst = new BinarySearchTree();
-bst.push(3);
-bst.push(2);
-bst.push(4);
-bst.push(1);
-bst.push(5);
+    }else if(node.left){
+      node.prev.left = node.left
+    }
+  }
+  //receives a method of traversal, a callback function and an optional node
+  traverse(method, callback, node){
+    //in-order = go node by node down the tree using the callback function on the node data
+    if(method === 'in-order'){
+      if(node === undefined){
+        let node = this.firstCenterNode
+        callback(node.data)
+        if(node.left !== null){
+          this.traverse(method, callback, node.left)
+        }
+        if(node.right !== null){
+          this.traverse(method, callback, node.right)
+        }
+      }else{
+        callback(node.data)
+        if(node.left !== null){
+          this.traverse(method, callback, node.left)
+        }
+        if(node.right !== null){
+          this.traverse(method, callback, node.right)
+        }
+      }
+    }else if(method === 'pre-order'){
+
+    }else if(method === 'post-order'){
+
+    }
+    //pre-order = enacts callback function on node data then recursive call to both left and right
+    //post-order = recursive call to left and right then enacts callback function on node data
+  }
+  //returns the total number of tree nodes in the tree
+  count(){
+    return this.numOfTreeNodes
+  }
+}
