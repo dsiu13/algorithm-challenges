7c0040b4951355f287c0ac24f0408edbaccbf32c
IamZombiez <dereksiu216@gmail.com>
2018-06-21 14:18:52 -0700

    updated comments

--- dailyCodingProblem/6-21-18.js
+++ dailyCodingProblem/6-21-18.js
@@ -1,9 +1,12 @@
 // Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 // For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
-// Bonus: Can you do this in one pass?
+// Bonus: Can you do this in one pass? 1 Hour time limit
 
 // Probably the worst way to do this.
 // Going to be super slow on large data sets.
+// Also fails if num in 1st array position doesn't sum to K with any other num in the array.
+
+// 1st Attempt
 var Array = [10, 15, 3, 7];
 var K = 17;
 var ansArray = [];
