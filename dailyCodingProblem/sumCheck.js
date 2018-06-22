685800e56af20dde6bb4949c6331a9dc467031a8
IamZombiez <dereksiu216@gmail.com>
2018-06-22 16:40:00 -0700

    i don't even

--- dailyCodingProblem/sumCheck.js
+++ dailyCodingProblem/sumCheck.js
@@ -22,4 +22,32 @@ function sumCheck(Array, K) {
 
 sumCheck(Array, K)
 
-// 2nd Attempt 6-22-18
+// 2nd Attempt 6-22-18 O(n^2)
+
+// our two sum function which will return
+// all pairs in the array that sum up to S
+function twoSum(arr, S) {
+
+  var sums = [];
+
+  // check each element in array
+  for (var i = 0; i < arr.length; i++) {
+
+    // check each other element in the array
+    for (var j = i + 1; j < arr.length; j++) {
+
+      // determine if these two elements sum to S
+      if (arr[i] + arr[j] === S) {
+        sums.push([arr[i], arr[j]]);
+      }
+    }
+  }
+
+  // return all pairs of integers that sum to S
+  return sums;
+
+};
+
+twoSum([3, 5, 2, -4, 8, 11], 7);
+
+// 3rd Attempt 6-23-18 Use a Hash Table...
