a4f5f27b2589c37291679ff2d444e452e5e0e024
IamZombiez <dereksiu216@gmail.com>
2018-06-29 16:54:58 -0700

    update

---
+++ practice-problems/reverseNum.js
@@ -0,0 +1,18 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+
+// Native JS methods
+function reverseInt(num){
+  var int = num.toString()
+  var reversedNum = Number(int.split('').reverse().join(''));
+  return reversedNum
+}
+reverseInt(123)
