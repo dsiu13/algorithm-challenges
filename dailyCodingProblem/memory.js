48e85d7f6ce2ad3dfd8d9a8b8e49e5d8a48bbfed
IamZombiez <dereksiu216@gmail.com>
2018-07-23 21:54:53 -0700

    added questions

--- dailyCodingProblem/memory.js
+++ dailyCodingProblem/memory.js
@@ -1,2 +1,2 @@
-// Given a stream of elements too large to store in memory, 
+// Given a stream of elements too large to store in memory,
 // pick a random element from the stream with uniform probability.
