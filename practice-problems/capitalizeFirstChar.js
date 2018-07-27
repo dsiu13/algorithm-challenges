2eb600b7584a129003b61ce9a5c820163f80d589
IamZombiez <dereksiu216@gmail.com>
2018-07-27 12:07:08 -0700

    2nd solution

--- practice-problems/capitalizeFirstChar.js
+++ practice-problems/capitalizeFirstChar.js
@@ -18,6 +18,34 @@
 
 function capitalize(str) {
   const words = [];
+
+  for(let word of str.split(' ')){
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return words.join(' ');
+}
+
+capitalize("hello there");
+
+
+// 2nd solution
+// create an empty string called 'result' =>
+// instead create 'result' which is the first char of the input string capitalized
+// for each char in the string
+// IF the character to the left of a spaces - capitalize it and add it to 'results'
+// ELSE add it to 'result'
+
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for(let i = 1; i < str.length: i++){
+    if(str[i-1] === ' '){
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
 }
 
 capitalize("hello there");
