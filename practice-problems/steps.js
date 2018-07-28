33c9e0138e1d535ce69995912475492253f6bfc6
IamZombiez <dereksiu216@gmail.com>
2018-07-28 10:27:02 -0700

    steps with recursion

--- practice-problems/steps.js
+++ practice-problems/steps.js
@@ -17,6 +17,47 @@
 //       '### '
 //       '####'
 
+// from 0 to n => create an empty string, "stair"
+// from 0 to n => IF the current column is =< than current row -> add '#' to stair
+// ELSE add a space to 'stair'
+// Console.log 'stair'
+
 function steps(n) {
+  for(let row = 0; row < n; row++) {
+    let stair = '';
+
+    for(let col = 0; col < n; col++){
+      stair += '#';
+    } else {
+      stair += ' ';
+    }
+    console.log(stair)
+  }
+}
+
+
+// Recursion
+// Figure out the bare min pieces of info to represent your problem
+// Give reasonable defaults to the bare min pieces of info
+// check base case. If theres no more work, return
+// do work, call function again, making sure the args have changed in some way
+
+function steps(n, row = 0, stair = '') {
+  if(n === row){
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    steps(m, row + 1);
+  }
+
+  if(stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  steps(n, row, stair);
 
 }
