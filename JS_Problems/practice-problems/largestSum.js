0b29059c236e5ac36ffe6ea3f3eeb23678b20385
IamZombiez <dereksiu216@gmail.com>
2019-02-21 12:50:52 -0800

    stuff

--- JS_Problems/practice-problems/largestSum.js
+++ JS_Problems/practice-problems/largestSum.js
@@ -5,3 +5,37 @@
 // [5, 1, 1, 5] should return 10, since we pick 5 and 5.
 //
 // Follow-up: Can you do this in O(N) time and constant space?
+
+largestNum = (arr) => {
+  let sum1 = 0
+  let sum2 = 0
+  for (i = 0; i < arr.length; i += 2) {
+    sum1 += arr[i]
+  };
+
+    for (i = 1; i < arr.length; i += 2) {
+    sum2 += arr[i]
+  };
+
+  if(sum1 > sum2){
+    return sum1
+  } else {
+    return sum2
+  }
+}
+
+largestNum([1,2,3,4,5])
+
+
+largestNum2 = arr => {
+  let arr1 = [];
+  let sum1 = 0;
+  let sum2 = 0;
+
+  function sumArr(arr) {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    for (i = 0; i < arr.length; i += 2) {
+      arr1.push(arr[i])
+    };
+    console.log(arr1.reduce(reducer))
+  }
