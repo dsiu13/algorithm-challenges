22aa138b433a93c9cbae5e269f769d22e113db9b
IamZombiez <dereksiu216@gmail.com>
2019-03-11 19:21:08 -0700

    1st attempt

--- JS_Problems/practice-problems/stocks.js
+++ JS_Problems/practice-problems/stocks.js
@@ -1,7 +1,7 @@
 // Say you have an array for which the ith element is the price of a given stock on day i.
 //
 // Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
-// 
+//
 // Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
 //
 // Example 1:
@@ -22,3 +22,18 @@
 // Input: [7,6,4,3,1]
 // Output: 0
 // Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+stocks = arr => {
+  let low = 4;
+  let day = 0;
+  arr.map(x => {
+    if ( x <= low ){
+      low = x
+      day = arr.indexOf(x)
+    }
+  });
+  console.log(low)
+  console.log(day)
+}
+
+stocks([1,2,3])
