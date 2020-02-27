d3a94d2f333e22c2fc26676d26937e9b1e502177
IamZombiez <dereksiu216@gmail.com>
2020-02-26 18:21:04 -0800

    added a sort solution

--- JS_Problems/leetcode/lastStoneWeight.js
+++ JS_Problems/leetcode/lastStoneWeight.js
@@ -36,3 +36,16 @@ lastStone = (rocks) => {
     return rocks[0] || 0;
 
 };
+
+// Sort solution
+var lastStoneWeight = function(stones) {
+    if(!stones || stones.length == 0) return 0
+
+    while(stones.length > 1) {
+        stones.sort((a, b) => b - a)
+        let x = stones.shift() - stones.shift()
+        stones.push(x)
+    }
+
+    return stones[0]
+};
