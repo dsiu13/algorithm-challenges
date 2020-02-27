0a7e0f87ae38c0bc0e0ba8ce3b6115d8f80f7e19
IamZombiez <dereksiu216@gmail.com>
2020-02-26 18:18:53 -0800

    last stone

---
+++ JS_Problems/leetcode/steps_to_zero.js
@@ -0,0 +1,4 @@
+// While loop to determine if a number is odd or even.
+// if Even divide by 2 else minus 1 then div by 2
+// Each pass through the loop adds to the step counter.
+// Help function to determine odd or even?
