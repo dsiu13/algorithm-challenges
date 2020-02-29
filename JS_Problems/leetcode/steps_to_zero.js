73d56d13e6c0205e24a8b4e17b77de3aa9490a4e
IamZombiez <dereksiu216@gmail.com>
2020-02-28 20:00:50 -0800

    i forgot how to do things

--- JS_Problems/leetcode/steps_to_zero.js
+++ JS_Problems/leetcode/steps_to_zero.js
@@ -1,5 +1,6 @@
 // 1342. Number of Steps to reduce a Number to Zero
-// Given a non-negative integer num, return the number of steps to reduce it to zero. If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+// Given a non-negative integer num, return the number of steps to reduce it to zero. If the current number is even, you have to divide it by 2,
+// otherwise, you have to subtract 1 from it.
 
 // Example 1:
 //
@@ -25,3 +26,19 @@
 //
 // Input: num = 123
 // Output: 12
+
+let zero = (num) => {
+  let steps = 0;
+  let res = num;
+
+  while ( res > 0) {
+    if(res % 2 == 0) {
+      steps += 1
+      res = res / 2
+    } else {
+      steps += 1
+      res = res - 1
+    };
+  };
+  return steps
+};
