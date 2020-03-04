2ef08dcc12674c882c5822dfd0436dfc0e5a9503
IamZombiez <dereksiu216@gmail.com>
2020-03-03 16:32:14 -0800

    it's wrong

---
+++ JS_Problems/leetcode/twoSums.js
@@ -0,0 +1,13 @@
+let twoSums = (arr, target) => {
+  let res = [];
+  while (i < arr.length - 1) {
+    i = 0
+    arr.map(x + arr[i + 1] === target){
+      if(x + arr[i + 1]){
+          res.push(indexOf(x))
+          res.push(indexOf(arr[i + 1]))
+      }
+    }
+    i++
+  }
+}
