7c42027246426914c3d59a46a094659e260c4748
IamZombiez <dereksiu216@gmail.com>
2020-03-04 18:57:39 -0800

    wow im bad at this

--- JS_Problems/leetcode/twoSums.js
+++ JS_Problems/leetcode/twoSums.js
@@ -10,20 +10,35 @@
 // Because nums[0] + nums[1] = 2 + 7 = 9,
 // return [0, 1].
 
-let twoSums = (arr, target) => {
-  let res = [];
-  let i = 0
-  while (i < arr.length - 1) {
-    arr.map(x => {
-      if(x + arr[i + 1] === target){
-          res.push(arr.indexOf(x))
-          res.push(arr.indexOf(arr[i + 1]))
-      }
-    });
-
-    i++
-  };
-  return res
-};
+// failed test cases 2 elements array, array with mulitple solutions
+// let twoSums = (arr, target) => {
+//   let res = [];
+//   let i = 0
+//   while (i < arr.length - 1) {
+//     arr.map(x => {
+//       if(x + arr[i + 1] === target){
+//           res.push(arr.indexOf(x))
+//           res.push(arr.indexOf(arr[i + 1]))
+//       }
+//     });
+//
+//     i++
+//   };
+//   return res
+// };
 
 twoSums([2, 7, 11, 15], 9)
+
+// The right way to do it....
+// map look up is 0(1)
+var twoSum = (nums, target) => {
+  const map = new Map()
+  for(let i = 0; i < nums.length; i++){
+    let val = nums[i];
+    if(map.has(c=val)){
+      return [map.get(val), i];
+    }
+    let diff = target - val
+    map.set(diff, i);
+  };
+};
