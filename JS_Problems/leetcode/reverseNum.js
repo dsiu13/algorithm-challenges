819781b897fee94ac417c44382035300bf64bde0
IamZombiez <dereksiu216@gmail.com>
2021-08-13 12:27:37 -0700

    maybe?

--- JS_Problems/leetcode/reverseNum.js
+++ JS_Problems/leetcode/reverseNum.js
@@ -2,10 +2,28 @@
 // Given a 32-bit signed integer, reverse digits of an integer.
 //
 // Note:
-// Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231,  231 − 1].
+// store integers within the 32-bit signed integer range: [−231,  231 − 1].
 // For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 
-let reverseNumber = x => {
+// let reverseNumber = x => {
+//   let reversedNum = [];
+//   let numStr = x.toString().split("");
+//   let i = numStr.length - 1;
+//
+//   numStr.map(x => {
+//     if (x == "-"){
+//       reversedNum.push(x)
+//     } else {
+//       reversedNum.push(numStr[i])
+//       i--
+//     }
+//   });
+//   let ans = parseInt(reversedNum.join(""));
+//   return ans;
+// };
+
+
+var reverse = function(x) {
   let reversedNum = [];
   let numStr = x.toString().split("");
   let i = numStr.length - 1;
@@ -19,5 +37,11 @@ let reverseNumber = x => {
     }
   });
   let ans = parseInt(reversedNum.join(""));
-  return ans;
+
+  if (ans > -2^31 || ans < (2^31 - 1)){
+    return 0;
+  } else {
+    return ans
+  }
+
 };
