858ebf4a55903a8587b106e3cba91e2597bbb908
IamZombiez <dereksiu216@gmail.com>
2021-08-19 11:54:58 -0700

    idk what im doing wrong

--- JS_Problems/interview_cake/JS/linkedListCycle.js
+++ JS_Problems/interview_cake/JS/linkedListCycle.js
@@ -1,10 +1,74 @@
-// mine
+//
 class Node {
 	constructor(data, next = null) {
-		this.data = data;
-		this.next = next;
+		  this.data = data;
+		  this.next = next;
 	}
 };
 
-const n1 = new Node(123)
-console.log(n1)
+class LinkedList
+{
+	constructor() {
+		this.head = null;
+		this.size = 0;
+	};
+
+		insertFirst(data)
+		{
+			this.head = new Node(data,this.head);
+			this.size++;
+		};
+
+			printListData()
+			{
+				let current = this.head;
+				while(current) {
+					console.log(current.data);
+					current = current.next;
+				};
+			};
+
+				insertLast(data)
+				{
+					let node = new Node(data)
+					let current;
+
+					if(!this.head) {
+						this.head = node;
+					} else {
+						current = this.head;
+							while (current.next) {
+								current = current.next;
+							}
+							current.next = node;
+					};
+							this.size++
+
+				};
+
+
+	insertLast(data)
+	{
+		let node = new Node(data);
+		let current;
+
+		if(!this.head){
+			this.head = node;
+		} else {
+				current = this.head;
+				while(current.next) {
+					current = current.next;
+			  }
+				this.size++;
+			};
+	 };
+
+};
+
+const ll = new LinkedList()
+ll.insertFirst(1);
+ll.insertFirst(2);
+ll.insertFirst(3);
+ll.insertLast(8);
+
+ll.printListData();
